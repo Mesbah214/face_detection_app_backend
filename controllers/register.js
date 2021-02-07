77ae645c6a0674db4e331cf26651200ff8b12d3f
Mesbah <msbh214@gmail.com>
2021-02-07 19:25:17 +0600

    put register inside controllers

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,31 @@
-const handleRegister = (req, res, bcrypt) => {};
+const handleRegister = (req, res, knex, bcrypt) => {
+  const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  knex.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+      .catch((err) => res.status(400).json("unable to register"));
+  });
+};
 
 module.exports = {
   handleRegister: handleRegister,
--- server.js
+++ server.js
@@ -46,32 +46,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  knex.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .then(trx.commit)
-          .catch(trx.rollback)
-          .catch((err) => res.status(400).json("unable to register"));
-      });
-  });
+  register.handleRegister(req, res, knex, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
