4257a5c334d8391e6260e52033fd096cd75f6697
Mesbah <msbh214@gmail.com>
2021-02-08 23:39:23 +0600

    changed the db host

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require("./controllers/image");
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-cubed-83063",
     user: "zero",
     password: "",
     database: "smart_brain",
